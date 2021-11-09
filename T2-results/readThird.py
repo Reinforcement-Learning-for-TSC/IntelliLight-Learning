115870066392f843070d6d4811e10de648145384
Sylvia <68999900+qwer-1234-q@users.noreply.github.com>
2021-11-09 22:07:32 +1100

    upload third files

---
+++ T2-results/readThird.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+
+file = open('third.txt', "r")
+data = file.readlines()
+
+data_rewards = []
+
+def get_number(split_row):
+	time = 0
+	reward = 0
+	index_time = split_row.index('time')
+	index = 0
+	for v in split_row:
+		if 'reward' in v:
+			reward = float(split_row[index+2].split('\t')[0])
+			break
+		index += 1
+	time = float(split_row[index_time+2].split('\t')[0])
+	return {'time': time, 'reward': reward}
+
+
+for row in data:
+	split_row = row.split(' ')
+	if 'time' in split_row:
+		data_rewards.append(get_number(split_row))
+
+final = []
+for value in data_rewards:
+	if value not in final:
+		final.append(value)
+
+x_values = []
+y_values = []
+for v in final:
+	x_values.append(v['time'])
+	y_values.append(v['reward'])
+
+if __name__ == "__main__":
+	print(data_rewards)
+	plt.figure()
+	plt.title('third - time vs reward')
+	plt.plot(x_values, y_values, c='green')
+	plt.tick_params(axis='both', which='major', labelsize=12)
+	plt.xlabel('time', fontsize=12)
+	plt.ylabel('reward', fontsize=12)
+	plt.show()
